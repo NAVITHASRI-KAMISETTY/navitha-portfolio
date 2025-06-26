1427468279740de3274346c7eb3b31e0e59b8492
NAVITHASRI-KAMISETTY <navithasrik@gmail.com>
2025-06-26 14:26:51 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.cv-download-btn').onclick = () => {
+  alert("CV download feature coming soon!");
+};
+
+document.querySelector('.get-in-touch-btn').onclick = () => {
+  alert("Redirecting to contact section...");
+};
